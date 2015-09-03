9ecfc265f63221407ae9e37f9ca42ae53e926b48
LakshaJael <nihongotraveler@icloud.com>
2015-09-03 19:05:43 +0900

    Changed a lot. Added files. Server stuff, as well as site development. Unfinished for now.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,60 @@
 $(document).ready(function() {
+  //var html = '';
+  
+
   $('.toggle-nav').click(function(e) {
     
     $(this).toggleClass('active');
-    $('.menu ul').toggleClass('active');
-
-   $('.arrow').toggleClass('rotate'); 
+    $('.menu ul').toggleClass('active'); 
 
     e.preventDefault();
   });
 
+  $('li a').click(function(event){
+    $('.toggle-nav').prop('value', this.text);
+    $('.menu ul').toggleClass('active');
+    $('.toggle-nav').toggleClass('active');
+    var beach = $('.toggle-nav').prop('value');    
+    var beachHTML = $("#beach_info").html(function()  {
+      var Html ='';
+      Html += $("#beach_info").append($("<div id='titleBar'/>"));
+      //clear the area!
+      $( "#titleBar" ).empty();
+      //Title bar for beach name
+      Html += $('#titleBar').append(beach + " Surf Report");
+      //Surf info. Divided into date, surf, swell, wind, weather         
 
- $('form').submit(function (evt) {
+                       
+      Html += makeTable($('#titleBar'), data1);
+      //Timestamp
+      var myDate = new Date(resultsData.timestamp *1000); 
+      $('.CSSTableGenerator').before($("<div id='dateDiv'/>"));
+      $('#dateDiv').append(myDate.toGMTString());
+      
+          
+     
+
+        
+  });
+
+
+
+  
+  
+
+
+
+    
+  })
+
+
+
+
+
+
+
+
+   $('form').submit(function (evt) {
     evt.preventDefault();
     var $searchTerm = $('#search');
     var $submitButton = $('#submit');
@@ -21,10 +65,10 @@ $(document).ready(function() {
    
     // the AJAX part
     //surfTwAPI link: placeholder
-    var surfTwAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var search = $searchTerm.val();
+    var surfTwAPI = "";
+    
     var surfOptions = {
-      tags: search,
+      
       format: "json"
     };
     function displaySurf(data) {
